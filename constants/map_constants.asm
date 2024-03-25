464b9b9932ba878bd3066c8bd9790e9d2c10bcbb
xaerochill <enosnemeis@gmail.com>
2024-03-25 20:12:00 +0100

    Visual Updates to Mount Moon

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -184,8 +184,8 @@ ENDM
 	map_const SEAFOAM_ISLANDS_B2F,                         15,  9 ; 119
 	map_const SEAFOAM_ISLANDS_B3F,                         17, 11 ; 120
 	map_const SEAFOAM_ISLANDS_B4F,                         17, 11 ; 121
-	map_const MOUNT_MOON_B1F,                              14, 14 ; 122
-	map_const MOUNT_MOON_B2F,                              20, 18 ; 123
+	map_const MOUNT_MOON_B1F,                              14, 17 ; 122
+	map_const MOUNT_MOON_B2F,                              22, 19 ; 123
 	map_const UNDERGROUND_NORTH,                            7, 20 ; 124
 	endgroup
 
